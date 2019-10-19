17142b75286ab46720ef538777ccb7b2dd4f6f1b
Gregoire Geis <git@gregoirege.is>
2019-10-19 22:02:27 +0200

    Initial gh-pages commit.

---
+++ precache-manifest.2431b100d604f081e0f65a060817ef90.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c91006e9ce74ea961a",
+    "url": "/./index.bundle.js"
+  },
+  {
+    "url": "/./index.fd2837f5f201b7b2d654.css"
+  },
+  {
+    "revision": "f0dc14e56a5715dd3ccfa01e8eb1adc6",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "ec4f9cecfae67c64df45b2a024fdcd81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a71ab9a62ac5febc899571556e1e79",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "44ceaf64d825308ab4539ab51c6d519e",
+    "url": "/manifest.webmanifest"
+  }
+]);
